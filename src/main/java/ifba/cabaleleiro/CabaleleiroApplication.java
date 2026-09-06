f4da99834d9131ea83a4b4a2af9dc5f1f332b4b5
cfManoca <emanuelmessias1902@gmail.com>
2026-09-05 23:55:11 -0300

    comit inicial

---
+++ src/main/java/ifba/cabaleleiro/CabaleleiroApplication.java
@@ -0,0 +1,13 @@
+package ifba.cabaleleiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabaleleiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabaleleiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifba/cabaleleiro/CabaleleiroApplicationTests.java
@@ -0,0 +1,13 @@
+package ifba.cabaleleiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabaleleiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
